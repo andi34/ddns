1bf5aed89ad34ea3c6ca50958f0d3e77ea15a75c
Andreas Blaesius <andi@unlegacy-android.org>
2020-03-29 10:59:51 +0200

    Add bash file to use on a static IP server

---
+++ 6tunnel.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Setup 6tunnel for applications that don't speak IPv6
+#
+
+# Configuration
+new_ip_file=$HOME/.ddns_v6.addr
+known_ip_file=$HOME/.known_ddns_v6.addr
+old_ip=""
+
+PORTS=(
+    '80'
+    '443'
+)
+
+# Let the fun begin
+if [[ ! -z $1 ]]; then
+  new_ip=$1
+elif [ -e $new_ip_file ]; then
+  new_ip=`cat $new_ip_file`
+else
+  echo "No IP given or found!"
+  sleep 5
+  exit 0
+fi
+
+[ -e $known_ip_file ] && old_ip=`cat $known_ip_file`
+
+if [ "$new_ip" = "$old_ip" ]; then
+  echo "IPv6 address unchanged"
+  sleep 5
+  exit 1
+else
+  # Add new IP to known_ip_file
+  echo $new_ip > $known_ip_file
+
+  # Setup 6tunnel rules
+  for port in "${PORTS[@]}"; do
+    # Check if port is in use already, if yes kill the process using it
+    lsof -ti :${port} | xargs --no-run-if-empty kill -9
+
+    # Setup 6tunnel and let it by default listen on IPv6 address (IPv4 is default)
+    6tunnel ${port} ${new_ip} ${port} -6
+  done
+fi
